e7c077264861b041694f65ed6a2a8e37996e8449
thebao171997 <thebao171997@gmail.com>
2026-07-28 14:03:32 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/dashboard.yhctbl/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
